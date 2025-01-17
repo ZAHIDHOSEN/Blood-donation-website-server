811d52250489bf0d59f121c379fc5bb54e00da5a
ZAHIDHOSEN <zahidhosen203@gmail.com>
2025-01-18 00:17:28 +0600

    added requests post related apis

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
-
-
-
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1befr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,189 +27,164 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
     // collection
-    const districtCollection = client.db('bloodBD').collection('district');
-    const upazilaCollection = client.db('bloodBD').collection('upazila');
-    const usersCollection = client.db('bloodBD').collection('users');
-
-
+    const districtCollection = client.db("bloodBD").collection("district");
+    const upazilaCollection = client.db("bloodBD").collection("upazila");
+    const usersCollection = client.db("bloodBD").collection("users");
+    const requestsCollection = client.db("bloodBD").collection("requests");
 
     // jwt related apis
-    app.post('/jwt', async(req,res) =>{
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '5h'})
-      res.send({token});
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      res.send({ token });
+    });
 
-    })
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
 
-
-    // middlewares 
-    const verifyToken = (req, res, next) =>{
-       console.log('inside verify token', req.headers.authorization);
-
-       if(!req.headers.authorization){
-        return res.status(401).send({message: 'unauthorize access'})
-       
-
-       }
-       const token = req.headers.authorization.split(' ')[1]
-       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-        if(err){
-          return res.status(401).send({message: 'unauthorize access'})
-
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorize access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorize access" });
         }
         req.decoded = decoded;
+      });
 
-       })
+      next();
+    };
 
-       next();
+    // use verify admin
 
-    }
-
-    // use verify admin 
-
-    const verifyAdmin = async(req, res, next)=>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email: email};
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      const isAdmin = user?.role === 'admin';
-      if(!isAdmin){
-        return res.status(403).send({message: 'forbidden access'})
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
       }
       next();
-
-    }
-
+    };
 
     // user related apis
 
-    app.get('/users', verifyToken,verifyAdmin, async(req,res) =>{
-     
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
-      res.send(result)
+      res.send(result);
     });
 
-
-
-    app.post('/users', async(req, res)=>{
+    app.post("/users", async (req, res) => {
       const user = req.body;
 
-      const query = {email: user.email}
+      const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already exist', insertedId: null})
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
       }
       const result = await usersCollection.insertOne(user);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     // admin role
 
-    app.patch('/users/admin/:id',verifyToken, verifyAdmin, async(req,res) =>{
-      const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
-      const updatedDoc = {
-        $set: {
-          role: 'admin'
-
-        }
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updatedDoc);
+        res.send(result);
       }
-      const result = await usersCollection.updateOne(filter, updatedDoc)
-      res.send(result); 
-
-    })
+    );
 
     // admin
 
-    app.get('/users/admin/:email',verifyToken, async(req, res) =>{
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
-      if(email !== req.decoded.email){
-        return res.status(403).send({message: 'forbidden access'})
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
 
-      const query = {email: email};
-      const user = await usersCollection.findOne(query)
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
 
       let admin = false;
-      if(user){
-        admin = user?.role === 'admin'
+      if (user) {
+        admin = user?.role === "admin";
       }
-      res.send({admin});
+      res.send({ admin });
+    });
 
+    // profile page get apis
 
-
-    })
-
-
-  // profile page get apis
-
-
-    app.get('/users', async(req, res) =>{
-
+    app.get("/users/profile", async (req, res) => {
       const email = req.query.email;
-       const query = {email: email}
-      const result = await usersCollection.findOne(query)
-      res.send(result)
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
 
-    })
+    //  profile data related apis
 
-  //  profile data related apis
-    
-    app.patch('/users/:id', async(req,res) =>{
+    app.patch("/users/:id", async (req, res) => {
       const item = req.body;
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
           name: item.name,
-         
-          avatar:item.avatar,
+
+          avatar: item.avatar,
           district: item.district,
           upazila: item.upazila,
-          group: item.group
-        }
-      }
-      const result = await usersCollection.updateOne(filter, updatedDoc)
+          group: item.group,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
       res.send(result);
+    });
 
-    })
-
-
-  
-
-
-
-
-
-    // district related collection
-
-    app.get('/district', async(req, res) =>{
-      const cursor = districtCollection.find();
-      const result = await cursor .toArray();
-      res.send(result);
-
-    })
-
-
-    // upazila releted apis
-
-    app.get('/upazila',async(req,res) =>{
-      const cursor = upazilaCollection.find();
-      const result = await cursor .toArray();
+    // requests related apis
+    app.post('/requests', async(req, res) =>{
+      const request = req.body;
+     
+      const result = await requestsCollection.insertOne(request);
       res.send(result)
     })
 
+    // district related collection
 
+    app.get("/district", async (req, res) => {
+      const cursor = districtCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    
+    // upazila releted apis
 
-    
-
-
-
+    app.get("/upazila", async (req, res) => {
+      const cursor = upazilaCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -221,17 +192,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Blood donation campaign is running");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send('Blood donation campaign is running')
-})
-
-
-
-app.listen(port, () =>{
-    console.log(`This is blood donation website : ${port}`)
-
-})
-
-
+app.listen(port, () => {
+  console.log(`This is blood donation website : ${port}`);
+});
